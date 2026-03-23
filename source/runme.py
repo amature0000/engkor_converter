e0fd22293f378a9a6c23bc583e35e465799dfd06
Juhyunsoo <amature0000@naver.com>
2026-03-24 00:26:52 +0900

    init2

--- source/runme.py
+++ source/runme.py
@@ -2,6 +2,10 @@ import keyboard
 from state import State
 
 
+def print_infos():
+    print("https://github.com/amature0000/engkor_converter")
+    print("EKconverter ver 4.0.0")
+
 class EventHandler:
     def __init__(self, state:State):
         self.state = state
@@ -11,24 +15,34 @@ class EventHandler:
         self.exit_key = 'esc'
         self.color_table_key = 'end'
 
-    def on_key_press(self, event):
+    def event_handler(self, event):
+        if event.event_type == 'up': return True
+        return self.process(event.name)
+    
+    def process(self, event):
         """command process"""
         if event == self.toggle_key:
             self.typing = not self.typing
             self.state.clear()
+            return True
         elif event == self.exit_key:
             self.typing = False
             self.state.clear()
+            return True
             
-        if not self.typing: return
+        if not self.typing: return True
         """typing process"""
-        self.state.record(event)
+        result = self.state.record(event)
+        self.state.write()
+        return result
 
 def main():
+    print_infos()
     state = State()
-
     e = EventHandler(state)
-    keyboard.on_press(lambda event: e.on_key_press(event.name))
+
+    keyboard.hook(e.event_handler, suppress=True)
+    keyboard.wait()
 
 if __name__ == "__main__":
     main()
